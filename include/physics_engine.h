b84263a2bd79fa49fb3e4562afb3b471eb4adb73
Dhivakar.S <cs25b023@smail.iitm.ac.in>
2026-02-28 22:57:05 +0530

    implemented AddCircle

--- include/physics_engine.h
+++ include/physics_engine.h
@@ -4,13 +4,16 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 #include "physics_object.h"
+#include "physics_circle.h"
 
 class PhysicsEngine{
 private:
   std::vector<std::unique_ptr<PhysicsObject>> objects;
   
 public:
-  void addObject(std::unique_ptr<PhysicsObject> obj);
+  void addCircle(float radius, ShapeID ID,
+  sf::Vector2f pos, sf::Vector2f vel, sf::Vector2f acc,
+  float angle, float ang_acc, float ang_vel, PhysicalAttributes attr, bool isStatic);
   void update(sf::Time dt);
   void draw(sf::RenderWindow& window);
 };
